c3720d8287237c143dfe70fd690ada5cb48777fc
芳升 赖 <451598452@qq.com>
2022-05-25 22:29:34 +0800

    修补上一个commit未完成的 修改移送文档命名空间的问题

--- LearnBinding/BindingSource/ADOAsSource.xaml.cs
+++ LearnBinding/BindingSource/ADOAsSource.xaml.cs
@@ -1,19 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 using LearnBinding.Tools;
-namespace LearnBinding.BindingPath
+
+namespace LearnBinding.BindingSource
 {
     /// <summary>
     /// ADOAsSource.xaml 的交互逻辑
--- LearnBinding/BindingSource/LinQAsSource.xaml.cs
+++ LearnBinding/BindingSource/LinQAsSource.xaml.cs
@@ -1,22 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Data;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using System.Xml;
 using System.Xml.Linq;
 using LearnBinding.Tools;
 
-namespace LearnBinding.BindingPath
+namespace LearnBinding.BindingSource
 {
     /// <summary>
     /// LinQAsSource.xaml 的交互逻辑
